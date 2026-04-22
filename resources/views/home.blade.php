a4aa14c71c60d4726f0b87412d02d6010f8d41f3
sistemcoba-dev <farelm298@gmail.com>
2026-04-22 12:13:25 +0700

    first_commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+<div>
+   <a href="/">Home</a>
+    <a href="/kontak">kontak</a>
+    <a href="/profil">profil</a>
+    <h1>Selamat Datang di Halaman home</h1>
+    <h2>Ini adalah halaman home milik saya</h2>
+</div>
