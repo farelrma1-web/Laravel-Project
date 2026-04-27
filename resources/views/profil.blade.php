d34465d93b02424bd4dc91b97fd58056ef765689
sistemcoba-dev <farelm298@gmail.com>
2026-04-27 11:27:02 +0700

    tambahkan halaman profil

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -1,3 +1,16 @@
-<div>
-    <!-- An unexamined life is not worth living. - Socrates -->
-</div>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Profil</title>
+</head>
+<body>
+    <h1>Halaman Profil</h1>
+
+    <p>Nama: {{ $nama }}</p>
+    <p>NIM: {{ $nim }}</p>
+    <p>Prodi: {{ $prodi }}</p>
+    <p>Kampus: {{ $kampus }}</p>
+
+    <a href="/kontak">Ke Kontak</a>
+</body>
+</html>
